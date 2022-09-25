e376e46c97c1400682f99dd259aabd363f8a3510
HelloKittty123 <phuong0611200@gmail.com>
2022-09-25 12:26:34 +0700

    Xây dựng Logic phần Header #3

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -12,6 +12,7 @@ import 'tippy.js/dist/tippy.css';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import AccountItem from '~/components/AccountItem';
 import styles from './Search.module.scss';
+import { useDebounce } from '~/hooks';
 
 const cx = classNames.bind(styles);
 
@@ -20,6 +21,8 @@ function Search() {
     const [searchResult, setSearchResult] = useState([]);
     const [showRes, setShowRes] = useState(true);
     const [loading, setLoading] = useState(false);
+
+    const debounced = useDebounce(searchValue, 500);
 
     const inputRef = useRef();
 
@@ -34,7 +37,7 @@ function Search() {
     };
 
     useEffect(() => {
-        if (!searchValue.trim()) {
+        if (!debounced.trim()) {
             setSearchResult([]);
             return;
         }
@@ -43,7 +46,7 @@ function Search() {
 
         fetch(
             `https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(
-                searchValue,
+                debounced,
             )}&type=less`,
         )
             .then((res) => res.json())
@@ -54,7 +57,7 @@ function Search() {
             .catch(() => {
                 setLoading(false);
             });
-    }, [searchValue]);
+    }, [debounced]);
 
     return (
         <HeadlessTippy
