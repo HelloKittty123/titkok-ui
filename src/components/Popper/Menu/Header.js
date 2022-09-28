8a3e1874043c15c7d88b65501baddafd49ecab01
HelloKittty123 <phuong0611200@gmail.com>
2022-09-28 14:27:25 +0700

    Áp dụng PropTypes vào dự án

--- src/components/Popper/Menu/Header.js
+++ src/components/Popper/Menu/Header.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Tippy from '@tippyjs/react/headless';
@@ -17,5 +18,10 @@ function Header({ title, onBack }) {
         </header>
     );
 }
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+    onBack: PropTypes.func.isRequired,
+};
 
 export default Header;
